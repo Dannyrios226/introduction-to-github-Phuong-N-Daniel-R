59f04d415c48a69c450cdff63eafa731bc3053b3
MisaPNguyen <misa.phnguyen@gmail.com>
2025-05-16 19:00:54 -0700

    Update hello-world-Danny-R.cpp
    Update hello-world

--- GitHub-Lab/GitHub-Lab/hello-world-Danny-R.cpp
+++ GitHub-Lab/GitHub-Lab/hello-world-Danny-R.cpp
@@ -4,10 +4,11 @@
 //
 //  Created by Danny Rios on 5/15/25.
 //
+using namespace std;
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World!";
+    cout << "Hello World!";
     return 0;
 }
 #include "hello-world-Danny-R.hpp"
